da16f9165fa56f05301a98694ca12f3f52b1d773
Abdul <47030364+kaligan-uitoux@users.noreply.github.com>
2019-11-28 16:19:33 +0530

    updates

--- src/CustomerPkgServiceProvider.php
+++ src/CustomerPkgServiceProvider.php
@@ -14,9 +14,7 @@ class CustomerPkgServiceProvider extends ServiceProvider {
 		$this->loadRoutesFrom(__DIR__ . '/routes/web.php');
 		$this->loadRoutesFrom(__DIR__ . '/routes/api.php');
 		$this->loadMigrationsFrom(__DIR__ . '/database/migrations');
-		$this->loadViewsFrom(__DIR__ . '/views', 'customer-pkg');
 		$this->publishes([
-			__DIR__ . '/views' => base_path('resources/views/customer-pkg'),
 			__DIR__ . '/public' => base_path('public'),
 		]);
 	}
--- src/routes/api.php
+++ src/routes/api.php
@@ -1,6 +1,6 @@
 <?php
-Route::group(['namespace' => 'Abs\TaxPkg\Api', 'middleware' => ['api']], function () {
-	Route::group(['prefix' => 'tax-pkg/api'], function () {
+Route::group(['namespace' => 'Abs\CustomerPkg\Api', 'middleware' => ['api']], function () {
+	Route::group(['prefix' => 'customer-pkg/api'], function () {
 		Route::group(['middleware' => ['auth:api']], function () {
 			// Route::get('taxes/get', 'TaxController@getTaxes');
 		});
