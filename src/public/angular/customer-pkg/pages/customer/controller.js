92c99b1f04b88dcf633c6adcba89239a27e339e4
Abdul <abdul@uitoux.in>
2020-01-07 15:01:22 +0530

    u

--- src/public/angular/customer-pkg/pages/customer/controller.js
+++ src/public/angular/customer-pkg/pages/customer/controller.js
@@ -1,3 +1,21 @@
+app.config(['$routeProvider', function($routeProvider) {
+
+    $routeProvider.
+    //CUSTOMER
+    when('/customer-pkg/customer/list', {
+        template: '<customer-list></customer-list>',
+        title: 'Customers',
+    }).
+    when('/customer-pkg/customer/add', {
+        template: '<customer-form></customer-form>',
+        title: 'Add Customer',
+    }).
+    when('/customer-pkg/customer/edit/:id', {
+        template: '<customer-form></customer-form>',
+        title: 'Edit Customer',
+    });
+}]);
+
 app.component('customerList', {
     templateUrl: customer_list_template_url,
     controller: function($http, $location, HelperService, $scope, $routeParams, $rootScope, $location) {
