4b286226324cb8949f5a2df4fcb7119569205f24
Suganya <suganya@uitoux.in>
2021-07-02 13:25:22 +0530

    chnages

--- src/database/migrations/2021_06_24_110942_add_dob_in_customer.php
+++ src/database/migrations/2021_06_24_110942_add_dob_in_customer.php
@@ -1,36 +1,33 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-class AddDobInCustomer extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        if (!Schema::hasColumn('customers', 'dob')) {
-            Schema::table('customers', function (Blueprint $table) {
-                $table->date('dob')->nullable()->after('itr_status')->comment('Used in VIMS');
-            });
-        }
-    }
+class AddDobInCustomer extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		if (!Schema::hasColumn('customers', 'dob')) {
+			Schema::table('customers', function (Blueprint $table) {
+				$table->date('dob')->nullable()->after('customer_status')->comment('Used in VIMS');
+			});
+		}
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        if (Schema::hasColumn('customers', 'dob')) {
-            Schema::table('customers', function (Blueprint $table) {
-                $table->dropColumn('dob');
-            });
-        }
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		if (Schema::hasColumn('customers', 'dob')) {
+			Schema::table('customers', function (Blueprint $table) {
+				$table->dropColumn('dob');
+			});
+		}
+	}
 }
